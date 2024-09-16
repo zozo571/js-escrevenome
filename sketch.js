2d465a4964c8151bde476c2db2d0edb9d85c75bf
zozo571 <gabriel.zorzanelo.santos@escola.pr.gov.br>
2024-09-16 16:48:56 -0300

    criação do arquivo index sketch style

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+    createCanvas(400, 400);
+    background("#067DF8")
+  }
+  
+  function draw() {
+    stroke("blue");
+    fill("red");
+    
+    
+    if (mouseIsPressed) {
+      rect(mouseX, mouseY, 20, 35);
+    }
+  }
+  
